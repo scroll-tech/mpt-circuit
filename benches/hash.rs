e6c0e8d4d3321e07a55c059c53ce3623f2a34db2
Ho Vei <noelwei@gmail.com>
2022-06-14 16:37:09 +0800

    evalulate the cost of initialization

--- benches/hash.rs
+++ benches/hash.rs
@@ -39,5 +39,9 @@ fn vec_ref(bench: &mut Bencher) {
     bench.iter(|| Vec::from(&RNDFRS.as_slice()[..8]));
 }
 
-benchmark_group!(hashes_bench, h02, h03, h04, h05, h06, h07, vec_ref);
+fn init_ref(bench: &mut Bencher) {
+    bench.iter(||  Hash::<Fr, P128Pow5T3<Fr>, ConstantLengthIden3<3>, 3, 2>::init());
+}
+
+benchmark_group!(hashes_bench, h02, h03, h04, h05, h06, h07, vec_ref, init_ref);
 benchmark_main!(hashes_bench);
