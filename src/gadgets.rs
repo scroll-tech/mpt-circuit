0ae1c3af85527b0ffaa7530dec4c98b9f07ecd8d
Mason Liang <mason@scroll.io>
2023-02-24 21:08:09 -0500

    wip

--- src/gadgets.rs
+++ src/gadgets.rs
@@ -56,3 +56,37 @@ impl ByteRangeCheckConfig {
         vec![meta.query_fixed(self.0, Rotation::cur())]
     }
 }
+
+struct ConstraintBuilder<F: Field> {
+    constraints: Vec<(
+        &'static str,
+        Box<dyn FnOnce(&mut VirtualCells<'_, F>) -> Expression<F>>,
+    )>,
+}
+
+impl<F: Field> ConstraintBuilder<F> {
+    fn add_constraint<T: IntoQuery<F>>(&mut self, name: &'static str, t: T) {
+        self.constraints.push((name, Box::new(t.into_query())))
+    }
+
+    fn build(self, cs: &mut ConstraintSystem<F>) {
+        for (name, query) in self.constraints {
+            cs.create_gate(&name, |meta| vec![query(meta)])
+        }
+    }
+}
+
+trait IntoQuery<F: Field>: Clone {
+    fn into_query(&self) -> Box<dyn FnOnce(&mut VirtualCells<'_, F>) -> Expression<F>>;
+}
+
+// enum Cell {
+//     One,
+//     Byte(u8),
+//     Add(Box<Cell>, Box<Cell>),
+//     Neg(Box<Cell>),
+//     Mul(Box<Cell>, Box<Cell>),
+//     Inv(Box<Cell>),
+//     Advice,
+//     Fixed,
+// }
