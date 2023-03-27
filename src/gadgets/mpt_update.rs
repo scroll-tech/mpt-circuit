439e4f9672bf22657adff1f8c2aa9d9ada00e692
Mason Liang <mason@scroll.io>
2023-03-27 16:04:29 -0400

    Implement MptUpdateLookup

--- src/gadgets/mpt_update.rs
+++ src/gadgets/mpt_update.rs
@@ -12,31 +12,23 @@ use halo2_proofs::{arithmetic::FieldExt, plonk::ConstraintSystem};
 
 pub trait MptUpdateLookup {
     fn lookup<F: FieldExt>(&self) -> [Query<F>; 7];
-    // old_root, F
-    // new_root, F
-    // old_value, rlc
-    // new_value, rlc
-    // proof_type, 0--10
-    // address, Address // get this from inputs to key hash
-    // storage_key, rlc // get this from inputs to key hash, along with
 }
 
 #[derive(Clone)]
 struct MptUpdateConfig {
     selector: SelectorColumn,
 
-    old_root: AdviceColumn, // can only change when depth = 0 and is_account_path is true
-    new_root: AdviceColumn, // can only change when depth = 0 and is_account_path is true
+    old_hash: AdviceColumn, // when depth = 0 and is_account_path, old_hash = old_root
+    new_hash: AdviceColumn, // when depth = 0 and is_account_path, new_hash = new_root
 
-    old_hash: AdviceColumn, // when depth = 0, old_hash = old_root
-    new_hash: AdviceColumn, // when depth = 0, new_hash = new_root
+    old_value_rlc: AdviceColumn,
+    new_value_rlc: AdviceColumn,
 
     proof_type: [BinaryColumn; 5],
 
-    address_hash: AdviceColumn, // poseideon hash of two halves of address
-    storage_key_hash: AdviceColumn, // poseidon hash of two halves of storage key
+    address: AdviceColumn,
+    storage_key_rlc: AdviceColumn,
 
-    // not used for lookups
     // exactly one of these is 1.
     is_common_path: BinaryColumn,
     old_hash_is_unchanged: BinaryColumn,
@@ -47,11 +39,45 @@ struct MptUpdateConfig {
     is_account_leaf: BinaryColumn,
     is_storage_path: BinaryColumn,
 
+    key: AdviceColumn,
     depth: AdviceColumn,
     depth_is_zero: IsZeroGadget,
     direction: AdviceColumn, // this actually must be binary because of a KeyBitLookup
 
     sibling: AdviceColumn,
+}
+
+impl MptUpdateLookup for MptUpdateConfig {
+    fn lookup<F: FieldExt>(&self) -> [Query<F>; 7] {
+        let is_root = || {
+            self.depth_is_zero
+                .current()
+                .and(self.is_account_path.current())
+        };
+        let old_root = self.old_hash.current() * is_root();
+        let new_root = self.new_hash.current() * is_root();
+        // let proof_type = self
+        //     .proof_type
+        //     .iter()
+        //     .enumerate()
+        //     .map(|(i, column)| column.current() * i)
+        //     .sum();
+        let proof_type = Query::one();
+        let old_value_rlc = self.new_value_rlc.current() * is_root();
+        let new_value_rlc = self.old_value_rlc.current() * is_root();
+        let address = self.address.current();
+        let storage_key_rlc = self.storage_key_rlc.current();
+
+        [
+            old_root,
+            new_root,
+            old_value_rlc,
+            new_value_rlc,
+            proof_type,
+            address,
+            storage_key_rlc,
+        ]
+    }
 }
 
 impl MptUpdateConfig {
@@ -63,15 +89,17 @@ impl MptUpdateConfig {
         rlc: &impl RlcLookup,
         bytes: &impl BytesLookup,
     ) -> Self {
-        let ([selector], [], [old_root, new_root, old_hash, new_hash]) = cb.build_columns(cs);
+        let ([selector], [], [old_hash, new_hash]) = cb.build_columns(cs);
 
         let proof_type = cb.binary_columns(cs);
-        let [address_hash, storage_key_hash] = cb.advice_columns(cs);
+        let [address, storage_key_rlc] = cb.advice_columns(cs);
+
+        let [old_value_rlc, new_value_rlc] = cb.advice_columns(cs);
 
         let [is_common_path, old_hash_is_unchanged, new_hash_is_unchanged] = cb.binary_columns(cs);
         let [is_account_path, is_account_leaf, is_storage_path] = cb.binary_columns(cs);
 
-        let [depth, direction, sibling] = cb.advice_columns(cs);
+        let [depth, key, direction, sibling] = cb.advice_columns(cs);
         let depth_is_zero = IsZeroGadget::configure(cs, cb, selector.current(), depth);
 
         // constrain that exactly one of proof type is 1.
@@ -98,11 +126,9 @@ impl MptUpdateConfig {
             selector.current(),
             depth.current() * (depth.current() - depth.previous() - 1),
         );
-        let key = address_hash.current() * is_account_path.current()
-            + storage_key_hash.current() * is_storage_path.current(); // + is_account_leaf...
         cb.add_lookup(
             "direction is correct for key and depth",
-            [key, depth.current(), direction.current()],
+            [key.current(), depth.current(), direction.current()],
             key_bit.lookup(),
         );
         let old_left = direction.current() * old_hash.previous()
@@ -118,21 +144,22 @@ impl MptUpdateConfig {
 
         Self {
             selector,
-            old_root,
-            depth_is_zero,
-            new_root,
             old_hash,
             new_hash,
             proof_type,
-            address_hash,
-            storage_key_hash,
+            old_value_rlc,
+            new_value_rlc,
+            address,
+            storage_key_rlc,
             is_common_path,
             old_hash_is_unchanged,
             new_hash_is_unchanged,
             is_account_path,
             is_account_leaf,
             is_storage_path,
+            key,
             depth,
+            depth_is_zero,
             direction,
             sibling,
         }
