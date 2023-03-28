9f44c8506cd897b39982b64eb2d28cbc8aeb0446
Mason Liang <mason@scroll.io>
2023-03-28 00:48:23 -0400

    Implement OneHot

--- src/gadgets.rs
+++ src/gadgets.rs
@@ -4,5 +4,5 @@ mod canonical_representation;
 mod is_zero;
 mod key_bit;
 mod mpt_update;
-// mod one_hot;
+mod one_hot;
 mod poseidon;
--- src/gadgets/one_hot.rs
+++ src/gadgets/one_hot.rs
@@ -1,52 +1,55 @@
+use crate::constraint_builder::BinaryQuery;
 use crate::constraint_builder::{AdviceColumn, BinaryColumn, ConstraintBuilder, Query};
 use halo2_proofs::{arithmetic::FieldExt, circuit::Region, plonk::ConstraintSystem};
 use itertools::Itertools;
 use std::fmt::Debug;
+use std::marker::PhantomData;
 use strum::IntoEnumIterator;
 
-// TODO: use EnumCount once generic_const_exprs is enabled.
-
 #[derive(Clone)]
-pub struct OneHot(Vec<BinaryColumn>);
+pub struct OneHot<T> {
+    // TODO: use [BinaryColumn; T::COUNT] once generic_const_exprs is enabled.
+    columns: Vec<BinaryColumn>,
+    phantom_data: PhantomData<T>,
+}
 
-impl OneHot {
-    pub fn configure<F: FieldExt, T: IntoEnumIterator + Eq>(
+impl<T: IntoEnumIterator + Eq> OneHot<T> {
+    pub fn configure<F: FieldExt>(
         cs: &mut ConstraintSystem<F>,
         cb: &mut ConstraintBuilder<F>,
     ) -> Self {
         let ([selector], [], []) = cb.build_columns(cs);
-
         let columns: Vec<_> = T::iter().map(|_| cb.binary_columns::<1>(cs)[0]).collect();
         cb.add_constraint(
-            "exactly one binary column is 1 in one hot encoding",
+            "exactly one binary column is set in one hot encoding",
             selector.current(),
             columns
                 .iter()
-                .map(|b| Query::from(b.current()))
-                .sum::<Query<F>>()
+                .fold(Query::zero(), |a, b| a.clone() + b.current())
                 - 1,
         );
-        Self(columns)
+        Self {
+            columns,
+            phantom_data: PhantomData,
+        }
     }
 
-    pub fn assign<F: FieldExt, T: IntoEnumIterator + Eq>(
-        &self,
-        region: &mut Region<'_, F>,
-        offset: usize,
-        value: T,
-    ) {
-        for (variant, column) in T::iter().zip_eq(&self.0) {
+    pub fn assign<F: FieldExt>(&self, region: &mut Region<'_, F>, offset: usize, value: T) {
+        for (variant, column) in T::iter().zip_eq(&self.columns) {
             column.assign(region, offset, variant == value);
         }
     }
 
-    pub fn condition<F: FieldExt, T: IntoEnumIterator + Eq>(
-        &self,
-        cb: &mut ConstraintBuilder<F>,
-        add_constraints: &mut dyn FnMut(&mut ConstraintBuilder<F>, T) -> (),
-    ) {
-        for variant in T::iter() {
-            add_constraints(cb, variant);
-        }
+    pub fn matches<F: FieldExt>(&self, value: T) -> BinaryQuery<F> {
+        T::iter()
+            .zip_eq(&self.columns)
+            .find_map(|(variant, column)| {
+                if variant == value {
+                    Some(column.current())
+                } else {
+                    None
+                }
+            })
+            .unwrap()
     }
 }
