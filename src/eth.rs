ea87f9542097f03773a3a63f7b793141689afe41
Zhang Zhuo <mycinbrin@gmail.com>
2023-05-17 12:36:27 +0800

    comment storage value lookup

--- src/eth.rs
+++ src/eth.rs
@@ -571,7 +571,8 @@ impl<'d, F: FieldExt> StorageChip<'d, F> {
         v_limbs: [Column<Advice>; 2],
         hash_table: &mpt::HashTable,
     ) -> StorageChipConfig {
-        meta.lookup_any("value hash", |meta| {
+        /*
+        meta.lookup_any("storage value hash", |meta| {
             let enable = meta.query_advice(s_enable, Rotation::cur());
             let fst = meta.query_advice(v_limbs[0], Rotation::cur());
             let snd = meta.query_advice(v_limbs[1], Rotation::cur());
@@ -579,6 +580,7 @@ impl<'d, F: FieldExt> StorageChip<'d, F> {
 
             hash_table.build_lookup(meta, enable, fst, snd, hash)
         });
+        */
 
         StorageChipConfig { v_limbs }
     }
