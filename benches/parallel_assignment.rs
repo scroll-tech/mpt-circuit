b1b3092743d4a9a5e96ce2d310aed75820c6c53a
Mason Liang <mason@scroll.io>
2023-11-19 11:55:11 -0500

    cleanup

--- benches/parallel_assignment.rs
+++ benches/parallel_assignment.rs
@@ -1,34 +1,23 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use halo2_mpt_circuits::MPTProofType;
 use halo2_mpt_circuits::{
-    gadgets::poseidon::PoseidonTable, serde::SMTTrace, tests::TestCircuit, MptCircuitConfig,
+    gadgets::poseidon::PoseidonTable, serde::SMTTrace, tests::TestCircuit, MPTProofType,
+    MptCircuitConfig,
 };
-use halo2_proofs::dev::MockProver;
-use halo2_proofs::halo2curves::bn256::{Bn256, Fr};
-use halo2_proofs::plonk::ConstraintSystem;
-use halo2_proofs::plonk::FirstPhase;
+use halo2_proofs::{
+    dev::MockProver,
+    halo2curves::bn256::{Bn256, Fr},
+    plonk::ConstraintSystem,
+};
 
-fn bench(c: &mut Criterion) {
+fn bench(criterion: &mut Criterion) {
     let json = include_str!("traces.json");
     let witness: Vec<(MPTProofType, SMTTrace)> = serde_json::from_str(&json).unwrap();
     let circuit = TestCircuit::new(10_000, witness);
 
-    c.bench_function("assign trace", |b| {
-        b.iter(|| MockProver::<Fr>::run(14, &circuit, vec![]))
+    criterion.bench_function("assign trace", |bencher| {
+        bencher.iter(|| MockProver::<Fr>::run(14, &circuit, vec![]))
     });
 }
-
-// fn criterion_benchmark(c: &mut Criterion) {
-//     let json = include_str!("traces.json");
-//     let trace: SMTTrace = serde_json::from_str(&json).unwrap();
-
-//     let mut cs = ConstraintSystem::default();
-//     let poseidon = PoseidonTable::configure(&mut cs);
-//     let challenge = cs.challenge_usable_after(FirstPhase);
-//     let mpt = MptCircuitConfig::configure(&mut cs, challenge, &poseidon);
-
-//     c.bench_function("assign trace", |b| b.iter(|| mpt.assign((black_box(20))));
-// }
 
 criterion_group! {
     name = benches;
